8e83f9fe5fbba919aae744413860e0ec78aea19a
PoojaPolepalli26 <164603015+PoojaPolepalli26@users.noreply.github.com>
2024-05-23 10:38:17 +0530

    Add files via upload

---
+++ pattern 5.py
@@ -0,0 +1,25 @@
+r=5
+for i in range(1,5):
+   for s in range(1,r-i+1):
+        print(" ",end=" ")
+    
+   for  j in range(1,2*i):
+        print("*",end=" ")
+   print()
+
+
+
+##r=5
+##for i in range(1,5):
+##    for s in range(1,r-i+1):
+##        print(" ",end=" ")
+##    for j in range(1,2*i): 
+##        if i==2 or j==3 or j==5 or i==3 or j==2 or j==6:
+##            
+##            print("*",end=" ")
+##        else:
+##            print(" ",end=" ")
+##            
+##    print()
+##        
+##           
