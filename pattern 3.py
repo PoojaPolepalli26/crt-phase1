3136e1c4120ecb8ecf6aeb94885a27cfe1ecd1e9
PoojaPolepalli26 <164603015+PoojaPolepalli26@users.noreply.github.com>
2024-05-23 10:37:25 +0530

    Add files via upload

---
+++ pattern 3.py
@@ -0,0 +1,10 @@
+##for i in range(1,6):
+##    for j in range (i-1,5):
+##        print("*",end="")
+##    print()
+
+for i in range(1,6):
+    for j in range(5,i-6):
+        print(end="","*")
+    print()
+ 
