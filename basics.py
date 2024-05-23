0ca934a1889208459a8ed0a7cc4e516db6dcf130
PoojaPolepalli26 <164603015+PoojaPolepalli26@users.noreply.github.com>
2024-05-23 10:23:58 +0530

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,30 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(15*25)
+375
+>>> print(23+54)
+77
+>>> print(15/3)
+5.0
+>>> print(56-76)
+-20
+>>> a=15
+>>> b=25
+>>> c=a*b
+>>> print(c)
+375
+>>> a=23
+>>> b=56
+>>> c=a+b
+>>> print(c)
+79
+>>> a=15
+>>> b=3
+>>> c=a/b
+>>> print (c)
+5.0
+>>> a=56
+>>> b=45
+>>> c=a-b
+>>> print(c)
+11
